a3997671bc08a89fa214342982250f7dfb5d5370
kush2863 <70647570+kush2863@users.noreply.github.com>
2025-09-08 07:32:57 +0530

    build errors solved

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -7,9 +6,6 @@ const nextConfig: NextConfig = {
     // Allow builds and dev to proceed even if ESLint reports errors.
     ignoreDuringBuilds: true,
   },
-  // When your repository has multiple lockfiles or monorepo layout,
-  // set this to the project root so Next picks the correct tracing root.
-  outputFileTracingRoot: path.join(__dirname, '..'),
 };
 
 export default nextConfig;
